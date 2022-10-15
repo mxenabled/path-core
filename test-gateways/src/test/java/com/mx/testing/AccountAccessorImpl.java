0573a603632acdbb4ea834736b3cf5dee0375b50
Matt Nichols <Matthew.Nichols@mx.com>
2022-10-14 18:19:10 -0600

    fix: set clientid in request context if not present
    Set the clientId on the GatewayRequestContext if not already set.
    This is done for convenience, since the gateway is built with the
    client as part of it's context. Requiring this to be set on the
    request context is redundant.

--- test-gateways/src/test/java/com/mx/testing/AccountAccessorImpl.java
+++ test-gateways/src/test/java/com/mx/testing/AccountAccessorImpl.java
@@ -3,8 +3,11 @@ package com.mx.testing;
 import lombok.Setter;
 
 import com.mx.common.accessors.AccessorConfiguration;
+import com.mx.common.accessors.AccessorResponse;
+import com.mx.common.accessors.PathResponseStatus;
 import com.mx.testing.accessors.AccountBaseAccessor;
 import com.mx.testing.accessors.TransactionBaseAccessor;
+import com.mx.testing.model.Account;
 
 public class AccountAccessorImpl extends AccountBaseAccessor {
   @Setter
@@ -18,5 +21,11 @@ public class AccountAccessorImpl extends AccountBaseAccessor {
   @Override
   public TransactionBaseAccessor transactions() {
     return transactions;
+  }
+
+  @Override
+  public AccessorResponse<Account> get(String id) {
+    Account account = new Account();
+    return AccessorResponse.<Account>builder().result(account).status(PathResponseStatus.OK).build();
   }
 }
---
+++ test-gateways/src/test/java/com/mx/testing/StaticContextCaptureBehavior.java
@@ -0,0 +1,32 @@
+package com.mx.testing;
+
+import com.mx.common.accessors.AccessorResponse;
+import com.mx.common.collections.ObjectMap;
+import com.mx.path.gateway.behavior.GatewayBehavior;
+import com.mx.path.gateway.context.GatewayRequestContext;
+
+/**
+ * Used in tests to observe context as requests pass through the gateway.
+ */
+public class StaticContextCaptureBehavior extends GatewayBehavior {
+  public static GatewayRequestContext capturedContext;
+
+  public static void reset() {
+    capturedContext = null;
+  }
+
+  public StaticContextCaptureBehavior(ObjectMap configurations) {
+    super(configurations);
+  }
+
+  public StaticContextCaptureBehavior(ObjectMap configurations, GatewayBehavior nextBehavior) {
+    super(configurations, nextBehavior);
+  }
+
+  @Override
+  protected <T> AccessorResponse<T> call(Class<T> resultType, GatewayRequestContext request, GatewayBehavior terminatingBehavior) {
+    StaticContextCaptureBehavior.capturedContext = request;
+
+    return callNext(resultType, request, terminatingBehavior);
+  }
+}
