53bb4b08f5481b501fb4c9833d8a498e56ca863d
Greg Felix <greg.felix@mx.com>
2022-10-27 10:34:19 -0600

    build: change exception name

--- common/src/main/java/com/mx/common/accessors/RequestValidationException.java
+++ common/src/main/java/com/mx/common/accessors/RequestValidationException.java
@@ -7,12 +7,12 @@ import com.mx.common.exception.PathRequestException;
  *
  * <p>See {@link PathRequestException} for usage details
  */
-public class InvalidDataException extends AccessorUserException {
-  public InvalidDataException(String message, String userMessage) {
+public class RequestValidationException extends AccessorUserException {
+  public RequestValidationException(String message, String userMessage) {
     super(message, userMessage, PathResponseStatus.USER_ERROR);
   }
 
-  public InvalidDataException(String message, String userMessage, Throwable cause) {
+  public RequestValidationException(String message, String userMessage, Throwable cause) {
     super(message, userMessage, PathResponseStatus.USER_ERROR, cause);
   }
 }
