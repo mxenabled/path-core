18dfe757e1cb3c88ec8be3861fef118e4d41cafe
Greg Felix <greg.felix@mx.com>
2022-10-27 11:24:45 -0600

    feat: adding two new AccessorUserExceptions build: applying formatting fixes and lockfiles build: change exception name build: spotless fix

--- common/src/main/java/com/mx/common/exception/PathRequestException.java
+++ common/src/main/java/com/mx/common/exception/PathRequestException.java
@@ -10,8 +10,10 @@ import com.mx.common.accessors.AccessorException;
 import com.mx.common.accessors.AccessorMethodNotImplementedException;
 import com.mx.common.accessors.AccessorSystemException;
 import com.mx.common.accessors.AccessorUserException;
+import com.mx.common.accessors.BadRequestException;
 import com.mx.common.accessors.PathResponseStatus;
 import com.mx.common.accessors.RequestPayloadException;
+import com.mx.common.accessors.RequestValidationException;
 import com.mx.common.accessors.ResourceNotFoundException;
 import com.mx.common.accessors.ResponsePayloadException;
 import com.mx.common.accessors.UnauthorizedException;
@@ -40,6 +42,8 @@ import com.mx.common.messaging.MessageError;
  *     {@link AccessorUserException} - Thrown on user-related errors in accessor code
  *       {@link ResourceNotFoundException} - Thrown when a resource is requested that does not exist
  *       {@link UnauthorizedException} - Thrown when a user attempts an operation when they are not authenticated, the session is expired, or the session is in a bad state
+ *       {@link BadRequestException} - Thrown when a request is malformed or missing required data
+ *       {@link RequestValidationException} - Thrown when a request is correctly formed, but the data is invalid for some reason
  *     {@link AccessorSystemException} - Thrown on unrecoverable error in accessor code
  *       {@link AccessorMethodNotImplementedException} - Thrown when an accessor method is invoked that has no implementation
  *       {@link RequestPayloadException} - Thrown when an upstream request payload cannot be built
