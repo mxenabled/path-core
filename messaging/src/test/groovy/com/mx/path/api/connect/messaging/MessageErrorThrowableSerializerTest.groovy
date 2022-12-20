c8146f830024f91886ffca34bfb5818ec2e7dbfe
Greg Felix <greg.felix@mx.com>
2022-12-19 17:38:26 -0700

    fix: return a known exception test: adding a unit test feat: adding RemoteException fix: spotless apply fix: fill in stack trace fix: cleanup throw text

--- messaging/src/test/groovy/com/mx/path/api/connect/messaging/MessageErrorThrowableSerializerTest.groovy
+++ messaging/src/test/groovy/com/mx/path/api/connect/messaging/MessageErrorThrowableSerializerTest.groovy
@@ -1,8 +1,10 @@
 package com.mx.path.api.connect.messaging
 
 import com.google.gson.GsonBuilder
+import com.mx.common.accessors.PathResponseStatus
 import com.mx.common.messaging.MessageError
 import com.mx.common.messaging.MessageStatus
+import com.mx.common.messaging.RemoteException
 
 import spock.lang.Specification
 
@@ -20,7 +22,27 @@ class MessageErrorThrowableSerializerTest extends Specification {
     def deserialized = gson.fromJson(serialized, MessageResponse.class)
 
     then:
-    deserialized.getException().getClass() == MessageError
-    ((MessageError) deserialized.getException()).messageStatus == MessageStatus.DISABLED
+    deserialized.exception.class == MessageError
+    ((MessageError) deserialized.exception).messageStatus == MessageStatus.DISABLED
+  }
+
+  def "test MessageErrorThrowableSerializer unknown cause"() {
+    given:
+    def gson = new GsonBuilder().registerTypeAdapter(Throwable.class, new MessageErrorThrowableSerializer()).create()
+    def responseJson = "{\"status\":\"FAIL\",\"exception\":{\"headers\":{},\"message\":\"The credentials provided were invalid.\",\"report\":true,\"status\":\"USER_ERROR\",\"userMessage\":\"The credentials provided were invalid.\",\"detailMessage\":\"The credentials provided were invalid.\",\"stackTrace\":[],\"suppressedExceptions\":[],\"_type\":\"anUnknownExceptionType\"}}"
+    MessageResponse response = MessageResponse.fromJson(responseJson)
+
+    when:
+    def serialized = gson.toJson(response)
+    def deserialized = gson.fromJson(serialized, MessageResponse.class)
+
+    then:
+    deserialized.status == MessageStatus.FAIL
+    deserialized.exception.class == RemoteException
+    ((RemoteException) deserialized.exception).message == "The credentials provided were invalid."
+    ((RemoteException) deserialized.exception).userMessage == "The credentials provided were invalid."
+    ((RemoteException) deserialized.exception).shouldReport()
+    ((RemoteException) deserialized.exception).status == PathResponseStatus.USER_ERROR
+    ((RemoteException) deserialized.exception).originalType == "anUnknownExceptionType"
   }
 }
