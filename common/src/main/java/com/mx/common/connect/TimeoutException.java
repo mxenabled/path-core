fd7dc690c61ce8b6481e8a6087522676f1518df8
Matt Nichols <Matthew.Nichols@mx.com>
2023-01-26 11:57:40 -0700

    fix: update connect exception statuses and set codes
    Updating statuses on connection exceptions to bring them in line with
    upstream system failure standards. Should respond with a 531. Setting
    code to previous status codes to help provide more information about
    the failure.

--- common/src/main/java/com/mx/common/connect/TimeoutException.java
+++ common/src/main/java/com/mx/common/connect/TimeoutException.java
@@ -10,17 +10,20 @@ import com.mx.common.exception.PathRequestException;
  */
 public class TimeoutException extends ConnectException {
   public TimeoutException() {
-    super("A request timeout occurred", PathResponseStatus.TIMEOUT);
+    super("A request timeout occurred", PathResponseStatus.UPSTREAM_SERVICE_UNAVAILABLE);
+    setCode(String.valueOf(PathResponseStatus.TIMEOUT.value()));
     setReport(false);
   }
 
   public TimeoutException(String message) {
-    super(message, PathResponseStatus.TIMEOUT);
+    super(message, PathResponseStatus.UPSTREAM_SERVICE_UNAVAILABLE);
+    setCode(String.valueOf(PathResponseStatus.TIMEOUT.value()));
     setReport(false);
   }
 
   public TimeoutException(String message, Throwable cause) {
-    super(message, PathResponseStatus.TIMEOUT, cause);
+    super(message, PathResponseStatus.UPSTREAM_SERVICE_UNAVAILABLE, cause);
+    setCode(String.valueOf(PathResponseStatus.TIMEOUT.value()));
     setReport(false);
   }
 }
