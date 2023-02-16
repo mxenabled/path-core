d66c9ee0a6f85b0380eff04f6e92f9b6fff2fc49
Matt Nichols <Matthew.Nichols@mx.com>
2023-02-16 14:40:38 -0700

    docs: fix TriConsumer javadocs

--- common/src/main/java/com/mx/common/lang/TriConsumer.java
+++ common/src/main/java/com/mx/common/lang/TriConsumer.java
@@ -10,19 +10,19 @@ public interface TriConsumer<T, U, V> {
    *
    * @param t the first input argument
    * @param u the second input argument
-   * @param v the third input parameter
+   * @param v the third input argument
    */
   void accept(T t, U u, V v);
 
   /**
-   * Returns a composed {@code BiConsumer} that performs, in sequence, this
+   * Returns a composed {@code TriConsumer} that performs, in sequence, this
    * operation followed by the {@code after} operation. If performing either
    * operation throws an exception, it is relayed to the caller of the
    * composed operation.  If performing this operation throws an exception,
    * the {@code after} operation will not be performed.
    *
    * @param after the operation to perform after this operation
-   * @return a composed {@code BiConsumer} that performs in sequence this
+   * @return a composed {@code TriConsumer} that performs in sequence this
    * operation followed by the {@code after} operation
    * @throws NullPointerException if {@code after} is null
    */
