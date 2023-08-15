c28d7d3712fa24ead46b43baba0ec631ba1ea7cd
Matt Nichols <Matthew.Nichols@mx.com>
2023-08-15 11:26:49 -0600

    feat(session): track keys saved to session store and DEL on logout
    This changes SessionRepositoryImpl by adding a tracking set for
    all keys set using Session.current().put()|sput()|putObj()|sputObj().
    On session delete, all of the keys in the set will be removed
    from the store along with the primary session object.

--- context/src/main/java/com/mx/path/core/context/store/SessionRepositoryImpl.java
+++ context/src/main/java/com/mx/path/core/context/store/SessionRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.mx.path.core.context.store;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -13,15 +14,21 @@ public class SessionRepositoryImpl implements SessionRepository {
   private static final Gson GSON = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, LocalDateTimeDeserializer.builder()
       .build()).create();
 
-  private Store store;
+  private final Store store;
 
   public SessionRepositoryImpl(Store store) {
     this.store = store;
   }
 
+  /**
+   * Delete session and associated data in store
+   *
+   * @param session
+   */
   @Override
   public final void delete(Session session) {
     store.delete(session.getId());
+    deleteSessionKeys(session);
   }
 
   @Override
@@ -40,6 +47,17 @@ public class SessionRepositoryImpl implements SessionRepository {
     return (json != null) ? GSON.fromJson(json, Session.class) : null;
   }
 
+  /**
+   * Keep track of all keys set using session put, sput, putObj, and sputObj
+   *
+   * <p>All of these keys will be removed when the session is removed with {@link #delete(Session)}
+   * @param session session
+   * @param key key to track
+   */
+  public final void registerSessionKey(Session session, String key) {
+    getScopedStoreSession(session).putSet("session_keys", key, session.getExpiresIn());
+  }
+
   @Override
   public final void save(Session session) {
     String json = GSON.toJson(session);
@@ -48,6 +66,7 @@ public class SessionRepositoryImpl implements SessionRepository {
 
   @Override
   public final void saveValue(Session session, String key, String value) {
+    registerSessionKey(session, key);
     getScopedStoreSession(session).put(key, value, session.getExpiresIn());
   }
 
@@ -61,8 +80,23 @@ public class SessionRepositoryImpl implements SessionRepository {
     return getScopedStoreSession(session).putIfNotExist(key, value, expiryMilliseconds);
   }
 
+  /**
+   * Deletes all tracked session keys for given session
+   *
+   * <p>Removes all keys found in "session_keys" and then removes the "session_keys" set
+   *
+   * @param session
+   */
+  private void deleteSessionKeys(Session session) {
+    Store sessionStore = getScopedStoreSession(session);
+    Set<String> keys = sessionStore.getSet("session_keys");
+    if (keys != null) {
+      keys.forEach(sessionStore::delete);
+    }
+    sessionStore.delete("session_keys");
+  }
+
   private Store getScopedStoreSession(Session session) {
     return new ScopedStoreSession(this.store, session);
   }
-
 }
