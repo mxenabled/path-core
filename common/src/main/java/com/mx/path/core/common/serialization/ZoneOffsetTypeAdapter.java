098f1ce9935d5a5026ecc9b3e167fb8027cd8246
Greg Felix <greg.felix@mx.com>
2023-08-30 14:26:17 -0600

    fix: adding OffsetDateTime and ZonedDateTime serializers

--- common/src/main/java/com/mx/path/core/common/serialization/ZoneOffsetTypeAdapter.java
+++ common/src/main/java/com/mx/path/core/common/serialization/ZoneOffsetTypeAdapter.java
@@ -1,22 +1,22 @@
 package com.mx.path.core.common.serialization;
 
 import java.io.IOException;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
-public class ZoneIdTypeAdapter extends TypeAdapter<ZoneId> {
+public class ZoneOffsetTypeAdapter extends TypeAdapter<ZoneOffset> {
   @Override
-  public final void write(JsonWriter out, ZoneId value) throws IOException {
+  public final void write(JsonWriter out, ZoneOffset value) throws IOException {
     if (value != null) {
       out.value(value.getId());
     }
   }
 
   @Override
-  public final ZoneId read(JsonReader in) throws IOException {
-    return ZoneId.of(in.nextString());
+  public final ZoneOffset read(JsonReader in) throws IOException {
+    return ZoneOffset.of(in.nextString());
   }
 }
