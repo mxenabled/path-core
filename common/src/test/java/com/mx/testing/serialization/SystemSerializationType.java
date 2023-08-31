6f2f0aac1ed1f4ec8d51609515cdf35bb4876fec
Matt Nichols <Matthew.Nichols@mx.com>
2023-08-30 19:24:39 -0600

    feat(serialization): add LocalDateTypeAdapter

--- common/src/test/java/com/mx/testing/serialization/SystemSerializationType.java
+++ common/src/test/java/com/mx/testing/serialization/SystemSerializationType.java
@@ -1,5 +1,10 @@
 package com.mx.testing.serialization;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.regex.Pattern;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,4 +15,16 @@ public class SystemSerializationType {
 
   @Builder.Default
   private String message = "I am a test";
+
+  @Builder.Default
+  private LocalDate localDate = null;
+
+  @Builder.Default
+  private LocalDateTime localDateTime = null;
+
+  @Builder.Default
+  private Duration duration = null;
+
+  @Builder.Default
+  private Pattern pattern = null;
 }
