fcab0bf4b4ffb05add2267f01e280acb6c18ae40
Matt Nichols <Matthew.Nichols@mx.com>
2023-12-18 12:18:45 -0700

    feat: add support for .env files
    This adds a new class Environment that combines system environment
    variables with those found in a .env file. It replaces the direct
    use of System.getenv in Configurator by using a custom
    StringSubstitutor StringLookup class that uses the new Environment
    class.

---
+++ context/src/main/java/com/mx/path/core/context/environment/Environment.java
@@ -0,0 +1,119 @@
+package com.mx.path.core.context.environment;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.mx.path.core.common.configuration.ConfigurationException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvBuilder;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
+/**
+ * Use this instead of {@link System#getenv(String)}
+ *
+ * System environment variables take precedence over variables set in the .env file. The .env file can be located in
+ * the resources directory, or in the project root directory.
+ */
+public class Environment {
+
+  private static Dotenv dotenv;
+  private static Path dotEnvLocation = null;
+  private static Map<String, String> dotEnv = null;
+  private static Map<String, String> all = null;
+
+  static Dotenv dotenv() {
+    if (dotenv == null) {
+      DotenvBuilder builder = Dotenv.configure().ignoreIfMissing();
+      if (dotEnvLocation != null) {
+        builder.filename(dotEnvLocation.toString());
+      }
+
+      dotenv = builder.load();
+    }
+    return dotenv;
+  }
+
+  static void setDotenv(Dotenv dotenv) {
+    Environment.dotenv = dotenv;
+  }
+
+  static void reset() {
+    dotEnvLocation = null;
+    dotenv = null;
+    all = null;
+    dotEnv = null;
+  }
+
+  /**
+   * Get environment variable value
+   * @param key case-sensitive key
+   * @return value
+   */
+  public static String get(String key) {
+    return dotenv().get(key);
+  }
+
+  /**
+   * Get environment variable value with default, if not set
+   * @param key case-sensitive key
+   * @param defaultValue default
+   * @return value
+   */
+  public static String get(String key, String defaultValue) {
+    return dotenv().get(key, defaultValue);
+  }
+
+  /**
+   * Set the location of the .env file. Only needed if file is in a non-standard location.
+   * @param location path to .env file
+   */
+  public static void setDotEnvLocation(String location) {
+    Path path = Paths.get(location);
+
+    if (!Files.exists(path)) {
+      throw new ConfigurationException(".env location does not exist. " + location);
+    }
+
+    dotEnvLocation = path;
+  }
+
+  /**
+   * @return Map of all environment variables (including those in ,env file)
+   */
+  public static Map<String, String> all() {
+    if (all == null) {
+      all = Collections.unmodifiableMap(dotenv()
+          .entries()
+          .stream()
+          .collect(Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue)));
+    }
+
+    return all;
+  }
+
+  /**
+   * @return Map of all environment variables found in the .env file
+   */
+  public static Map<String, String> dotEnv() {
+    if (dotEnv == null) {
+      dotEnv = Collections.unmodifiableMap(dotenv()
+          .entries(Dotenv.Filter.DECLARED_IN_ENV_FILE)
+          .stream()
+          .collect(Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue)));
+    }
+
+    return dotEnv;
+  }
+
+  /**
+   * @return Map of all system environment variables (not including those in ,env file)
+   */
+  public static Map<String, String> system() {
+    return System.getenv();
+  }
+}
--- gateway/src/main/java/com/mx/path/gateway/configuration/Configurator.java
+++ gateway/src/main/java/com/mx/path/gateway/configuration/Configurator.java
@@ -28,6 +28,7 @@ import com.mx.path.core.common.security.EncryptionService;
 import com.mx.path.core.common.serialization.ObjectMapJsonDeserializer;
 import com.mx.path.core.common.serialization.ObjectMapYamlDeserializer;
 import com.mx.path.core.common.store.Store;
+import com.mx.path.core.context.environment.EnvironmentStringSubstitutor;
 import com.mx.path.core.context.facility.Facilities;
 import com.mx.path.core.utility.reflection.ClassHelper;
 import com.mx.path.gateway.Gateway;
@@ -36,8 +37,6 @@ import com.mx.path.gateway.accessor.Accessor;
 import com.mx.path.gateway.behavior.GatewayBehavior;
 import com.mx.path.gateway.event.GatewayEventBus;
 import com.mx.path.gateway.service.GatewayService;
-
-import org.apache.commons.text.StringSubstitutor;
 
 /**
  * Base abstract class for Gateway Configurators.
@@ -112,7 +111,7 @@ public abstract class Configurator<T extends Gateway<?>> {
     GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(ObjectMap.class, new ObjectMapJsonDeserializer());
     Gson gson = gsonBuilder.create();
 
-    json = StringSubstitutor.createInterpolator().replace(json);
+    json = EnvironmentStringSubstitutor.replace(json);
     ObjectMap map = gson.fromJson(json, ObjectMap.class);
 
     return buildGateways(map);
@@ -122,8 +121,8 @@ public abstract class Configurator<T extends Gateway<?>> {
     if (Strings.isBlank(document)) {
       return new LinkedHashMap<>();
     }
-    document = StringSubstitutor.createInterpolator().replace(document);
 
+    document = EnvironmentStringSubstitutor.replace(document);
     ObjectMapYamlDeserializer yamlSerializer = new ObjectMapYamlDeserializer(ObjectMapYamlDeserializer.Parameters.builder()
         .maxYamlAliases(MAX_YAML_ALIASES)
         .build());
