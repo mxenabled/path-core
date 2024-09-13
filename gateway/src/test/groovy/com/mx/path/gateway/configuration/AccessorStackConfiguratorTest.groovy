0c286f243126d5f4771f6cb89d8b6fdd1a5cb238
Bruno Martins <bruno.martins@contractors.mx.com>
2024-09-13 13:35:20 -0300

    feat: add after initialize method on accessor

---
+++ gateway/src/test/groovy/com/mx/path/gateway/configuration/AccessorStackConfiguratorTest.groovy
@@ -0,0 +1,69 @@
+package com.mx.path.gateway.configuration
+
+import static org.mockito.Mockito.mockStatic
+import static org.mockito.Mockito.times
+
+import java.lang.reflect.Method
+
+import com.mx.path.core.common.accessor.AfterAccessorInitialize
+import com.mx.path.gateway.accessor.Accessor
+
+import org.mockito.MockedStatic
+
+import spock.lang.Specification
+
+class AccessorStackConfiguratorTest extends Specification {
+
+  class MockAccessor extends Accessor {
+    @AfterAccessorInitialize
+    static void afterInitialize() {
+    }
+  }
+
+  class ChildMockAccessor extends MockAccessor {
+    @AfterAccessorInitialize
+    static void afterInitializeChild() {
+    }
+  }
+
+  AccessorStackConfigurator subject
+  static MockedStatic<MockAccessor> mockAccessor
+  static MockedStatic<ChildMockAccessor> childMockAccessor
+
+  def setup() {
+    subject = new AccessorStackConfigurator(ConfigurationState.getCurrent())
+    mockAccessor.reset()
+    childMockAccessor.reset()
+  }
+
+  def setupSpec() {
+    mockAccessor = mockStatic(MockAccessor.class)
+    childMockAccessor = mockStatic(ChildMockAccessor.class)
+  }
+
+  def cleanupSpec() {
+    mockAccessor.close()
+    childMockAccessor.close()
+  }
+
+  def "invoke afterInitializeMethods"() {
+    when:
+    Method method = subject.class.getDeclaredMethod("invokeAfterInitializeMethods", Class)
+    method.setAccessible(true)
+    method.invoke(subject, MockAccessor.class)
+
+    then:
+    mockAccessor.verify({ MockAccessor.afterInitialize() }, times(1))
+  }
+
+  def "invoke both afterInitializeMethods"() {
+    when:
+    Method method = subject.class.getDeclaredMethod("invokeAfterInitializeMethods", Class)
+    method.setAccessible(true)
+    method.invoke(subject, ChildMockAccessor.class)
+
+    then:
+    mockAccessor.verify({ MockAccessor.afterInitialize() }, times(1))
+    childMockAccessor.verify({ ChildMockAccessor.afterInitializeChild() }, times(1))
+  }
+}
