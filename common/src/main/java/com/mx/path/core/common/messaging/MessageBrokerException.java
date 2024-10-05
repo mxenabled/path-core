0dc09e8a5c2c35d3eed02b33da943664aac1ea5d
Sathish Ramesh <sathish.ramesh@mx.com>
2024-10-05 20:38:21 +0530

    fix: handle internal error flag for NATs timeout error

--- common/src/main/java/com/mx/path/core/common/messaging/MessageBrokerException.java
+++ common/src/main/java/com/mx/path/core/common/messaging/MessageBrokerException.java
@@ -2,7 +2,9 @@ package com.mx.path.core.common.messaging;
 
 import com.mx.path.core.common.accessor.PathResponseStatus;
 import com.mx.path.core.common.exception.PathRequestException;
-
+/**
+ * All errors from NATS should be wrapped in this exception
+ */
 public class MessageBrokerException extends PathRequestException {
 
   public MessageBrokerException(String message, Throwable cause) {
